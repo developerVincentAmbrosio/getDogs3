a32f34aa650c24612986ebc82366fe93c7e0f6ba
developerVincentAmbrosio <70174185+developerVincentAmbrosio@users.noreply.github.com>
2020-11-03 12:21:54 -0500

    Add files via upload
    changes from mentor session

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-function getDogPic (hound) {
-  fetch(`https://dog.ceo/api/breed/${hound}/images/random`)
+function getDogPic (breed) {
+  fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
     .then(response => response.json())
     .then(responseJson => {
-      if (responseJson.status != "success") {
-        alert("Oops! we can't find that one. Please check the spelling or pick another breed.");
+      if (responseJson.status === "success") {
+        renderImageUrl(responseJson.message);
       } else {
-        renderImageUrl(responseJson.message);  
+        $('.js-dog-pic-container').empty();
+        alert("Oops! we can't find that one. Please check the spelling or pick another breed.");
       }
-    });
-}
+    })
+    .catch(error => alert("Oops! we can't find that one. Please check the spelling or pick another breed."));
+  }
 
 function getDogPicListener() {
   $('form').submit(event => {
     event.preventDefault();
-    const hound = $('.js-breed').val();
-    getDogPic(hound);
+    const breed = $('.js-breed').val();
+    getDogPic(breed);
   });
 }
 
 function renderImageUrl(url){
-  $('.js-dog-pic-container').replaceWith(dogPicTemplate(url)).html();
+  $('.js-dog-pic-container').empty().append(dogPicTemplate(url));
 }
 
 function dogPicTemplate(imageUrl) {
-  return `<img src="${imageUrl}" class="js-dog-pic-container">`;
+  return `<img src="${imageUrl}"/>`;
 }
 
 $(function() {
